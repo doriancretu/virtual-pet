90b476e57414b3b673c6f2daf662d38937a72649
doriancretu <dorianalin.cretu@yahoo.com>
2019-10-24 20:15:57 +0100

    added growUp method to pet

--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,16 @@
 
 
-function Pet() {};
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.growUp = function() {
+        this.age += 1;
+      };
+};
+const pet = {
+    name: 'Fido'
+  };
+  /* or const pet = {};
+   pet.name = 'Fido';*/
 
 module.exports = Pet;
