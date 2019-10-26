d5d4ba880d58bf863ecab7cc399c03b0fa48dd1d
doriancretu <dorianalin.cretu@yahoo.com>
2019-10-26 11:13:44 +0100

    increased fitness to a maximum of 10

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -16,6 +16,10 @@ describe('constructor', () => {
      const pet = new Pet('Fido');
      expect(pet.hunger).toEqual(0);
    });
+   it('should have initial fitness level of 10', () => {
+    const pet = new Pet('Fido');
+    expect(pet.fitness).toEqual(10);
+   })
 });
  describe('growUp', () => {
    it('increments the age by 1', () => {
@@ -29,3 +33,21 @@ describe('constructor', () => {
      expect(pet.hunger).toEqual(5);
    });
  });
+ describe('walk', () => {
+    it('increases fitness by 4', () => {
+      const pet = new Pet('fido');
+  
+      pet.fitness = 4;
+      pet.walk();
+  
+      expect(pet.fitness).toEqual(8);
+    });
+    it('increases fitness by to a maximum of 10', () => {
+        const pet = new Pet('fido');
+    
+        pet.fitness = 8;
+        pet.walk();
+    
+        expect(pet.fitness).toEqual(10);
+      });
+  });
--- src/pet.js
+++ src/pet.js
@@ -1,21 +1,32 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
     this.hunger = 0;
+    this.fitness = 10;
     // this.growUp = function() {
     //   this.age +=1;
     //}
  };
  Pet.prototype.growUp = function() {
-  this.age +=1;
-  this.hunger +=5;
- }
- // const pet = {
- //     name: ‘Fido’,
- //     age: 0,
- // };
- /* or const pet = {};
- pet.name = ‘Fido’; */
+  this.age += 1;
+  this.hunger += 5;
+ };
+ Pet.prototype.walk = function() {
+
+    if ((this.fitness + 4) <= 10 ) {
+        this.fitness += 4;
+      } else {
+        this.fitness = 10;
+      }
+    };
+//   const pet = {
+//       name: ‘Fido’,
+//       age: 0,
+//  };
+//  const pet = {};
+//  pet.name = ‘Fido’; 
  const fido = new Pet('Fido');
  const rex = new Pet('Rex');
  module.exports = Pet;
